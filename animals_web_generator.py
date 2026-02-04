a6b8f753dd9ea0976e74cdb64e8353584a0710e7
AlexReschke <alex.reschke@gmx.de>
2026-02-04 23:13:01 +0100

    „JTitel der Karten wird mittig angezeigt und jedes Karten-Feld wird in einem separaten Listenelement ausgegeben."

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -20,40 +20,43 @@ def get_type(t):
     else:
         return tpe
 
-def output_tier(t):
+def output_html(t):
     output = ""
 
     output += '<li class="cards__item">'
 
     name = get_name(t)
     if name is not None:
-        output += f'<div class="card__title">{name}</div>'
-        output += '<p class="card__text">'
+        output += f'<div class="card__title"><center>{name}</div>'
+        output += '<div class="card__text">'
+        output += '<ul>'
 
         diet = get_diet(t)
         if diet is not None:
-            output += f'<strong>Diet:</strong> {diet}<br/>'
+            output += f'<li><strong>Diet:</strong> {diet}</li>'
 
         first_loc = get_first_location(t)
         if first_loc is not None:
-            output += f'<strong>Location:</strong> {first_loc}<br/>'
+            output += f'<li><strong>Location:</strong> {first_loc}</li>'
 
         typ = get_type(t)
         if typ is not None:
-            output += f'<strong>Type:</strong> {typ}<br/>'
+            output += f'<li><strong>Type:</strong> {typ}</li>'
 
-        output += "</p>"
-    output += "</li>"
+        output += '</ul>'
+
+        output += '</p>'
+    output += '</li>'
 
     if output:
         print(output)
         return output
     return ""
 
-def print_tier(data):
+def iter_data(data):
     all_output = []
     for animal in data:
-        s = output_tier(animal)
+        s = output_html(animal)
         if s:
             all_output.append(s)
     return "\n".join(all_output)
@@ -65,7 +68,7 @@ def main():
     with open("animals_template.html", "r", encoding="utf-8") as f:
         html = f.read()
 
-        output = print_tier(data)
+        output = iter_data(data)
 
         new_html = html.replace("__REPLACE_ANIMALS_INFO__", output)
 
