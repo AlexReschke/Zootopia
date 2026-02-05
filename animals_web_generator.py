415bf1dd3ae8366ab3c23bf049c58f5897164ae7
AlexReschke <alex.reschke@gmx.de>
2026-02-05 09:28:08 +0100

    „Fehlende docstrings nachgetragen."

--- animals_web_generator.py
+++ animals_web_generator.py
@@ -1,18 +1,22 @@
 import json
 
 def get_name(t):
+    """get the name out of animal_data.json"""
     return t.get("name")
 
 def get_diet(t):
+    """get the diet out of animal_data.json"""
     ch = t.get("characteristics", {})
     return ch.get("diet")
 
 def get_first_location(t):
+    """get the location out of animal_data.json"""
     locs = t.get("locations")
     if isinstance(locs, list) and len(locs) > 0:
         return locs[0]
 
 def get_type(t):
+    """get the type out of animal_data.json"""
     ch = t.get("characteristics", {})
     tpe = ch.get("type")
     if tpe is None:
@@ -21,6 +25,7 @@ def get_type(t):
         return tpe
 
 def output_html(t):
+    """create the output list and format the html for the cards"""
     output = ""
 
     output += '<li class="cards__item">'
@@ -54,6 +59,7 @@ def output_html(t):
     return ""
 
 def iter_data(data):
+    """iterates through the data of animal_data.json"""
     all_output = []
     for animal in data:
         s = output_html(animal)
